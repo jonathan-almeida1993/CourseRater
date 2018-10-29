8b30a84c5037df468a4be873dbcd935eb6972083
Unknown <minic@onid.oregonstate.edu>
2018-10-29 16:54:50 -0700

    Made header appear on course page again.

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -5,35 +5,4 @@ $(document).ready(function() {
       $('#logoutModal').modal('show');
     }
   }
-
-  $('#loginBtn').click(function() {
-    if (validateLoginForm()) {
-      //JAVA CODE TO LOGIN
-    }
-  });
 });
-/* Login Form Validation */
-// function validateLoginForm() {
-// 	$('#badUsernameAlert').css("display", "none");
-// 	$('#badPasswordAlert').css("display", "none");
-//   var onidUsername = $('#onidUsername').val();
-//   var onidPassword = $('#onidPassword').val();
-// 	var formReady = true;
-//   if (onidUsername.length == 0) {
-//     $('#badUsernameAlert').html("Required field!");
-//     $('#badUsernameAlert').css("display", "block");
-// 		//$('#badUsernamePopover').popover();
-// 		$('#onidUsername').css("box-shadow", "0 0 10px red");
-// 		$('#onidPassword').css("box-shadow", "0 0 0 black");
-// 		formReady = false;
-//   }
-//   else if (onidPassword.length == 0) {
-//     $('#badPasswordAlert').html("Required field!");
-//     $('#badPasswordAlert').css("display", "block");
-// 		//$('#badPasswordPopover').popover();
-// 		$('#onidPassword').css("box-shadow", "0 0 10px red");
-// 		$('#onidUsername').css("box-shadow", "0 0 0 black");
-// 		formReady = false;
-//   }
-// 	return formReady;
-// }
