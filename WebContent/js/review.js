95870b24272bfe66859118effa0fba87c94c62e0
Omeed Habibelahian <habibelo@oregonstate.edu>
2018-11-03 17:08:09 -0700

    Fix functionality of Term and Instructor dropdowns on course page search
    Dropdowns now correctly save decisions made from course search on landing page/previous course page course search

--- WebContent/js/review.js
+++ WebContent/js/review.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
 	console.log("courseIdURL: " + courseIdURL);
 	var courseIds = courseIdURL.split('C');
 	courseIds.splice(0,1);
-	console.log("course IDs: [" + courseIds + "]");
 
 	console.log(document.cookie);
 	$('#studentName').val(document.cookie.split('=')[1].replace('-',' '));
@@ -86,22 +85,63 @@ $(document).ready(function(){
     $('#termDropDownCP').append('<option value="">All Terms</option>').val('');
     $('#instructorDropDownCP').find('option').remove();
     $('#instructorDropDownCP').append('<option value="">All Professors/Instructors</option>').val('');
-    jsonTermInstr = jQuery.parseJSON(termInstrList);
+    var jsonTermInstr = jQuery.parseJSON(termInstrList);
     console.log("jsonTermInstr: " + jsonTermInstr);
     
     var terms = [];
-    for (i = 0; i < jsonTermInstr.length; i++) {
-    	if (!terms.includes(jsonTermInstr[i].termOffered)) {
-    		terms.push(jsonTermInstr[i].termOffered);
-    	}
-    }	
-    
     var instructors = [];
-    for (i = 0; i < jsonTermInstr.length; i++) {
-    	if (!instructors.includes(jsonTermInstr[i].instructor)) {
-    		instructors.push(jsonTermInstr[i].instructor);
+	console.log("course IDs: [" + courseIds + "]");
+    $.each(jsonTermInstr, function(index, value) {
+    	console.log(value.courseId);
+    	
+    	console.log(value.termOffered);
+    	console.log(value.instructor);
+    	if (courseIds.includes(value.courseId.toString())) {
+    		if (!terms.includes(value.termOffered)) {
+    			terms.push(value.termOffered);
+    			console.log("terms now: " + terms);
+    		}
+    		if (!instructors.includes(value.instructor)) {
+    			instructors.push(value.instructor);
+    		}
     	}
-    } 
+    });
+    
+    $.each(terms, function(index, value) {
+        $('#termDropDownCP').append('<option value="'+value+'">'+value+'</option>');
+    });
+      
+    $.each(instructors, function(index, value) {
+        $('#instructorDropDownCP').append('<option value="' + value + '">' + value +'</option>');
+    });
+    
+    $('#termDropDownCP').val('');
+    console.log(terms);
+    if (terms.length == 1) {
+    	$('#termDropDownCP').val(terms[0]);
+    }
+    
+    $('#instructorDropDownCP').val('');
+    console.log(instructors);
+    if (instructors.length == 1) {
+    	$('#instructorDropDownCP').val(instructors[0]);
+    }
+    
+    /*for (i = 0; i < jsonTermInstr.length; i++) {
+    	console.log(jsonTermInstr[i].termOffered);
+        if (!terms.includes(jsonTermInstr[i].termOffered)) {
+        	terms.push(jsonTermInstr[i].termOffered);
+        }
+    }
+    for (i = 0; i < jsonTermInstr.length; i++) {
+    	console.log(jsonTermInstr[i].instructor)
+        if (!instructors.includes(jsonTermInstr[i].instructor)) {
+        	instructors.push(jsonTermInstr[i].instructor);
+        }
+    }
+    
+    console.log(terms);
+    console.log(instructors);
 
     $.each(terms, function(index, value) {
       $('#termDropDownCP').append('<option value="'+value+'">'+value+'</option>');
@@ -111,8 +151,15 @@ $(document).ready(function(){
       $('#instructorDropDownCP').append('<option value="' + value + '">' + value +'</option>');
     });
     
-    $('#termDropDownCP').val(courseDetailsJSON.termOffered);
-    $('#instructorDropDownCP').val(courseDetailsJSON.instructor);
+    $('#termDropDownCP').val('');
+    for (i = 0; i < courseIds.length; i++) {
+    	$('#termDropDownCP').val(terms[0]);
+    }
+    
+    $('#instructorDropDownCP').val('');
+    else {
+    	$('#instructorDropDownCP').val(instructors[0]);
+    }*/
 
 	/* When the user changes the subject in the Course Search form */
 	  $('#subjectDropDownCP').change(function(){
@@ -144,7 +191,7 @@ $(document).ready(function(){
 
 	  /* When the user changes the course number in the Course Search form */
 	  $('#courseDropDown').change(function(){
-	    var dept = $('#subjectDropDownCP').val();
+		var dept = $('#subjectDropDownCP').val();
 	    var course = $('#courseDropDownCP').val();
 	    var jsonData = '{"department":"'+dept+'","courseNo":"'+course+'"}';
 
@@ -169,19 +216,18 @@ $(document).ready(function(){
 	    console.log("jsonTermInstr: " + jsonTermInstr);
 	    
 	    var terms = [];
+	    var instructors = [];
+	    courseIds = [];
 	    for (i = 0; i < jsonTermInstr.length; i++) {
 	    	if (!terms.includes(jsonTermInstr[i].termOffered)) {
 	    		terms.push(jsonTermInstr[i].termOffered);
 	    	}
-	    }	
-	    
-	    var instructors = [];
-	    for (i = 0; i < jsonTermInstr.length; i++) {
 	    	if (!instructors.includes(jsonTermInstr[i].instructor)) {
 	    		instructors.push(jsonTermInstr[i].instructor);
 	    	}
-	    } 
-
+	    	courseIds.push(jsonTermInstr[i].courseId);
+	    }
+	    
 	    $.each(terms, function(index, value) {
 	      $('#termDropDownCP').append('<option value="'+value+'">'+value+'</option>');
 	    });
@@ -195,10 +241,14 @@ $(document).ready(function(){
 	  $('#termDropDownCP').change(function(){
 		var savedInstructor = $('#instructorDropDownCP').val();
 		var instructors = [];
+		courseIds = [];
 	    if ($('#termDropDownCP').val() == "") {
 	        for (i = 0; i < jsonTermInstr.length; i++) {
 	        	if (!instructors.includes(jsonTermInstr[i].instructor)) {
 	        		instructors.push(jsonTermInstr[i].instructor);
+	        	}
+	        	if (jsonTermInstr[i].instructor == savedInstructor || savedInstructor == "") {
+	            	courseIds.push(jsonTermInstr[i].courseId);
 	        	}
 	        }
 	    }
@@ -207,10 +257,14 @@ $(document).ready(function(){
 	        	console.log("term offered: " + jsonTermInstr[i].termOffered);
 	        	if (!instructors.includes(jsonTermInstr[i].instructor) && jsonTermInstr[i].termOffered == $('#termDropDownCP').val()) {
 	        		instructors.push(jsonTermInstr[i].instructor);
-	                console.log("instructors: " + instructors);
+	        	}
+	        	if (jsonTermInstr[i].termOffered == $('#termDropDownCP').val() && (jsonTermInstr[i].instructor == savedInstructor || savedInstructor == "")) {
+	        		courseIds.push(jsonTermInstr[i].courseId);
 	        	}
 	        }
 	    }
+	    console.log("instructors: " + instructors);
+	    console.log("course ids: " + courseIds);
 	    $('#instructorDropDownCP').find('option').remove();
 	    $('#instructorDropDownCP').append('<option value="">All Professors/Instructors</option>').val('');
 	    $.each(instructors, function(index, value) {
@@ -225,10 +279,14 @@ $(document).ready(function(){
 	  $('#instructorDropDownCP').change(function(){
 		  var savedTerm = $('#termDropDownCP').val();
 	  	  var terms = [];
+	  	  courseIds = [];
 		  if ($('#instructorDropDownCP').val() == "") {
 		        for (i = 0; i < jsonTermInstr.length; i++) {
 		        	if (!terms.includes(jsonTermInstr[i].termOffered)) {
 		        		terms.push(jsonTermInstr[i].termOffered);
+		        	}
+		        	if (jsonTermInstr[i].termOffered == savedTerm || savedTerm == "") {
+			        	courseIds.push(jsonTermInstr[i].courseId);
 		        	}
 		        }
 		  }
@@ -237,10 +295,15 @@ $(document).ready(function(){
 		        	console.log("instructor: " + jsonTermInstr[i].instructor);
 		        	if (!terms.includes(jsonTermInstr[i].termOffered) && jsonTermInstr[i].instructor == $('#instructorDropDownCP').val()) {
 		        		terms.push(jsonTermInstr[i].termOffered);
-		                console.log("terms: " + terms);
+		        	}
+		        	if (jsonTermInstr[i].instructor == $('#instructorDropDownCP').val() && (jsonTermInstr[i].termOffered == savedTerm || savedTerm == "")) {
+		        		courseIds.push(jsonTermInstr[i].courseId);
 		        	}
 		        }
+		        
 		  }
+	      console.log("terms: " + terms);
+	      console.log("course ids: " + courseIds);
 		  $('#termDropDownCP').find('option').remove();
 	      $('#termDropDownCP').append('<option value="">All Terms</option>').val('');
 	      $.each(terms, function(index, value) {
@@ -253,22 +316,26 @@ $(document).ready(function(){
 
 	  /* Action when clicking the Search Course button on the landing page */
 	  $('#searchCourseBtnCP').click(function() {
-	    //var res = validateSearchForm();
+		//var res = validateSearchForm();
 	    console.log("Search button pressed!");
 	    if(validateSearchForm()){
 	    	var dept = $('#subjectDropDownCP').val();
 	        var course = $('#courseDropDownCP').val();
 	        var term = $('#termDropDownCP').val() == "" ? "ALL" : $('#termDropDownCP').val();
 	        var instructor = $('#instructorDropDownCP').val() == "" ? "ALL" : $('#instructorDropDownCP').val();
+	        var courseIdParam = '';
+	        $.each(courseIds, function(index, value) {
+	        	courseIdParam += 'C' + value; 
+	        });
 
 	        var jsonData = '{"department":"'+dept+'","courseNo":"'+course+'","termOffered":"'+term+'","instructor":"'+instructor+'"}'; 
 	        console.log("json data: " + jsonData);
 	        
 	        // get Course ID
-	        var courseId = sendDataSync(jsonData,"fetchCourseID","CourseController");
-	        var jsonCourseId = jQuery.parseJSON(courseId);
-	        $('#searchCourseBtnCP').val(jsonCourseId.courseId);
-	        window.location.href = 'course_page.html?courseId='+$('#searchCourseBtnCP').val();
+	        //var courseId = sendDataSync(jsonData,"fetchCourseID","CourseController");
+	        //var jsonCourseId = jQuery.parseJSON(courseId);
+	        //$('#searchCourseBtn').val(jsonCourseId.courseId);
+	      window.location.href = 'course_page.html?courseId=' + courseIdParam;
 	    }
 	  });
 
