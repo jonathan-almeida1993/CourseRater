6932252a3f50c086379e3f4410bd35f424105bce
Unknown <minic@onid.oregonstate.edu>
2018-11-05 08:12:53 -0800

    Back top top button complete for course page and index.

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,3 +1,20 @@
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+	    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	        document.getElementById("backToTop").style.display = "block";
+	    } else {
+	        document.getElementById("backToTop").style.display = "none";
+	    }
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+	    document.body.scrollTop = 0;
+	    document.documentElement.scrollTop = 0;
+	}
+	
 $(document).ready(function() {
 
   //get department list
