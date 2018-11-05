2b927c47b5df94b0c35e665e3d560a34a4ebbb7c
Rogersyp <songyip@oregonstate.edu>
2018-11-04 16:27:00 -0800

    display reviews in webpages

--- WebContent/js/review.js
+++ WebContent/js/review.js
@@ -12,80 +12,100 @@ $(document).ready(function(){
 	$('#courseInstructorHeader').text(courseDetailsJSON.instructor);
 	$('#termTaken').val(courseDetailsJSON.termOffered);
 	$('#courseDesc').text(courseDetailsJSON.courseDesc);
-
-	/* Temporary dummy reviews. In the future, actual reviews will be retrieved in place of these. */
+	
+	
+	//var reviewList
+	//grab all reviews based on the course Ids, and put them into a list called reviesList
+	var reviews = [];	// array that will hold all the reviews for the class
+	
+	var jsonData = '{"courseId":"' + courseIdURL + '"}';
+	var reviewList = sendDataSync(jsonData, "getCourseReviews", "ReviewController");
+	
+	//parse the string for each review and put them into correct field of a review object
+	var jsonReviews = jQuery.parseJSON(reviewList);
+	
+	//add the review object into the review list and display them 
+	$.each(jsonReviews, function(index, value) {
+		reviews.push(value);
+	});
+	
+//	/* Temporary dummy reviews. In the future, actual reviews will be retrieved in place of these. */
 	var exampleRev1 = new Object();
-	exampleRev1.name = "Omeed Habibelahian";
+	exampleRev1.onid = "Omeed Habibelahian";
+	exampleRev1.anonymous = 0;
 	exampleRev1.rating = 4;
-	exampleRev1.term = "Fall 2017";
-	exampleRev1.datePosted = "10/01/2018";
+	exampleRev1.createdDate = "10/01/2018";
 	exampleRev1.gradeReceived = "A";
-	exampleRev1.reviewText = "160 was tough. They will cram a lot of information in "+
-	"your head in a very short time. However, her assignments are fun, and "+
-	"her approach on how to take coding and computer science is very intuitive "+
-	"and interesting rather than boring. She helps a lot and cares about the "+
-	"student. It may be hard but you\'ll learn the material well.";
-	var jsonExampleRev1 = JSON.stringify(exampleRev1);
-
+	exampleRev1.review = "160 was tough. They will cram a lot of information in your head in a very short time. However, her assignments are fun, and her approach on how to take coding and computer science is very intuitive and interesting rather than boring. She helps a lot and cares about the student. It may be hard but you'll learn the material well.";
+	
 	var exampleRev2 = new Object();
-	exampleRev2.name = "Anonymous Student";
+	exampleRev2.onid = "Anonymous Student";
+	exampleRev2.anonymous = 1;
 	exampleRev2.rating = 3;
-	exampleRev2.term = "Fall 2016";
-	exampleRev2.datePosted = "09/30/2018";
-	exampleRev2.grade = "B";
-	exampleRev2.reviewText = "*This student did not write a review in their submission.*";
-	var jsonExampleRev2 = JSON.stringify(exampleRev2);
-
-	var reviews = [];		// array that will hold all the reviews for the class
-	reviews.push(jsonExampleRev1, jsonExampleRev2);
-	var numStars = 0;
+	exampleRev2.createdDate = "09/30/2018";
+	exampleRev2.gradeReceived = "B";
+	exampleRev2.review = "";
+	
+	//var reviews = [];		// array that will hold all the reviews for the class
+	//reviews.push(exampleRev1, exampleRev2);
+	updateAverageRating(reviews);
+	
+	var reviewDiv = "";
 	var starHTML = "";
+	numStars = 0;
 	for (i = 0; i < reviews.length; i++) {
-		numStars = reviews[i]['rating'];
+		numStars = reviews[i].rating;
 		starHTML = "";
 		for (j = 0; j < numStars; j++) {
 			starHTML += "<img class=\"rating-star\" src=\"images/star-8x_full.png\">";
 		}
-		$('#currentReviews').html(
-			$('#currentReviews').html() +
+		if (reviews[i].anonymous) {
+			reviews[i].onid = "Anonymous Student";
+		}
+		if (reviews[i].review == "") {
+			reviews[i].review = "*This student did not write a review in their submission.*";
+		}
+	
+		$('#reviews').append(
 			"<div class=\"modal-body row\">" +
-	      "<div class=\"col-md-3\">" +
-	        "<label>" +
-	          "<strong>Posted By:</strong>" +
-	          "<span>" +
-	            reviews[i]['name'] +
-	          "</span>" +
-	        "</label><br>" +
-	        "<label>" +
-	          "<strong>Rating:</strong>" +
-	          "<span>" +
-	            starHTML +
-	          "</span>" +
-	        "</label><br>" +
-	        "<label>" +
-	          "<strong>Term:</strong>" +
-	          "<span>" +
-	            reviews[i]['term'] +
-	          "</span>" +
-	        "</label><br>" +
-	        "<label>" +
-	          "<strong>Date Posted:</strong>" +
-	          "<span>" +
-	            reviews[i]['datePosted'] +
-	          "</span>" +
-	        "</label><br>" +
-	        "<label>" +
-	          "<strong>Grade Received:</strong>" +
-	          "<span>" +
-	            reviews[i]['grade'] +
-	          "</span>" +
-	        "</label>" +
-	      "</div>" +
-	      "<div class=\"col-md-7\">" +
-	        reviews[i]['reviewText'] +
-	      "</div>" +
-	    "</div>" +
-	    "<span class=\"modal-header\"></span>");
+			"<div class=\"col-md-3\">" +
+			"<label>" +
+			"<strong>Posted By: </strong>" +
+			"<span>" +
+			reviews[i].onid +
+			"</span>" +
+			"</label><br>" +
+			"<label>" +
+			"<strong>Rating: </strong>" +
+			"<span>" +
+			starHTML +
+			"</span>" +
+			"</label><br>" +
+			"<label>" +
+			"<strong>Term: </strong>" +
+			"<span>" +
+			reviews[i].term +
+			"</span>" +
+			"</label><br>" +
+			"<label>" +
+			"<strong>Date Posted: </strong>" +
+			"<span>" +
+			reviews[i].createdDate +
+			"</span>" +
+			"</label><br>" +
+			"<label>" +
+			"<strong>Grade Received: </strong>" +
+			"<span>" +
+			reviews[i].gradeReceived +
+			"</span>" +
+			"</label>" +
+			"</div>" +
+			"<div class=\"col-md-7\">" +
+			reviews[i].review +
+			"</div>" +
+			"</div>" +
+			"<span + class=\"modal-header\"></span>"
+		);
 	}
 
 	$('#submitReviewBtn').click(function(){
@@ -98,6 +118,8 @@ $(document).ready(function(){
 		if (status == "JDBC_OK") {
 			$('#submitPendingAlert').css("display", "none");
 			$('#submitSuccessAlert').css("display", "block");
+		} else if (status == "INVALID_SESSION"){
+			window.location.href= 'login.html';
 		}
 	});
 	$('#dismissSubmitSuccessAlert').click(function () {
@@ -130,4 +152,35 @@ function toggleAnonymousMsg() {
       anonymousCheck.checked = true;
       $("#anonymousMsg").css("display", "block");
     }
+}
+
+function updateAverageRating(reviews) {
+	var averageRating = 0;
+	var ratingSum = 0;
+	console.log(reviews.length);
+	for (i = 0; i < reviews.length; i++) {
+		ratingSum += parseInt(reviews[i].rating);
+	}
+	console.log(ratingSum);
+	averageRating = parseFloat((ratingSum/reviews.length).toString().substring(0,4));
+	console.log(averageRating);
+	var numStars = averageRating;
+	var avgStarSpanHTML = "";
+	while (numStars > 0) {
+		if (numStars >= 1) {
+			avgStarSpanHTML += "<img class=\"rating-star\" src=\"images/star-8x_full.png\">";
+		}
+		else if (numStars >= 0.625 && numStars < 1) {
+			avgStarSpanHTML += "<img class=\"rating-star\" src=\"images/star-8x_three-quarter.png\">";
+		}
+		else if (numStars >= 0.375 && numStars < 0.625) {
+			avgStarSpanHTML += "<img class=\"rating-star\" src=\"images/star-8x_half.png\">";
+		}
+		else if (numStars < 0.375) {
+			avgStarSpanHTML += "<img class=\"rating-star\" src=\"images/star-8x_quarter.png\">";
+		}
+		numStars--;
+	}
+	$('#averageRatingStars').html(avgStarSpanHTML);
+	$('#averageRatingValue').html(averageRating + "/5");
 }
