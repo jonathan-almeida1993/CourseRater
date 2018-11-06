4c820431ed2df104fbe4eac68b7983ee69c348bc
Jonathan Almeida <almeida_jonathan@yahoo.com>
2018-11-06 12:31:52 -0800

    Refactor and fix Login Controller and test_view_review.js

--- src/com/osu/controller/LoginController.java
+++ src/com/osu/controller/LoginController.java
@@ -16,68 +16,65 @@ import com.osu.dao.base.impl.LoginDAOImpl;
 import com.osu.dao.base.interfaces.LoginDAO;
 import com.osu.database.pojo.UserPojo;
 
-
 public class LoginController extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String message = null;
 		String jsonData = null;
 		message = request.getParameter("message");
 		jsonData = request.getParameter("JDATA");
-		
-		
+
 		LoginDAO dao = null;
 		System.out.println("\n\nLoginController:doPost @@@@@ message :: " + message + " jsonData :: " + jsonData);
-		
+
 		if (null != message && CommonConstants.OP_VALIDATE_lOGIN.equalsIgnoreCase(message)) {
-			
+
 			UserPojo loginDetails = new UserPojo();
 			dao = new LoginDAOImpl();
 			loginDetails.setOnid(request.getParameter("onid"));
 			loginDetails.setPassword(request.getParameter("password"));
-			
-			System.out.println("ONID = "+loginDetails.getOnid());
-			System.out.println("password = "+loginDetails.getPassword());
+
+			System.out.println("ONID = " + loginDetails.getOnid());
+			System.out.println("password = " + loginDetails.getPassword());
 			System.out.println("VALIDATING USER LOGIN...");
-			
+
 			UserPojo userDetails = dao.validateUserLogin(loginDetails);
-			
-			
-			
-			if(CommonConstants.STATUS_AUTH_SUCCESS.equals(userDetails.getStatus())){
+
+			if (CommonConstants.STATUS_AUTH_SUCCESS.equals(userDetails.getStatus())) {
 				HttpSession session = request.getSession();
-				//setting session to expiry in 30 mins
-				session.setMaxInactiveInterval(30*60);
+				// setting session to expiry in 30 mins
+				session.setMaxInactiveInterval(30 * 60);
 				session.setAttribute("user", loginDetails.getOnid());
-				Cookie userName = new Cookie("user", userDetails.getFirstName() +"-"+userDetails.getLastName());
+				Cookie userName = new Cookie("user", userDetails.getFirstName() + "-" + userDetails.getLastName());
 				Cookie userOnid = new Cookie("onid", loginDetails.getOnid());
-				userName.setMaxAge(30*60);
+				userName.setMaxAge(30 * 60);
 				response.addCookie(userName);
 				response.addCookie(userOnid);
 				response.sendRedirect("index.html");
-			
-			}else{
+
+			} else {
 				RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-				PrintWriter out= response.getWriter();
-				//out.println("<font color=red>Either user name or password is wrong.</font>");
-				out.println("<div class=\"alert alert-danger\" role=\"alert\"> " + 
-						"Invalid Username or Password. Please try again.</div>");
+				PrintWriter out = response.getWriter();
+				// out.println("<font color=red>Either user name or password is wrong.</font>");
+				out.println("<div class=\"alert alert-danger\" role=\"alert\"> "
+						+ "Invalid Username or Password. Please try again.</div>");
 				rd.include(request, response);
 			}
-		}else if(null != message && CommonConstants.OP_LOGOUT.equalsIgnoreCase(message)) {
+		} else if (null != message && CommonConstants.OP_LOGOUT.equalsIgnoreCase(message)) {
 			HttpSession session = request.getSession(false);
-			if(session != null) {
+			if (session != null) {
 				session.invalidate();
 			}
 			response.sendRedirect("login.html");
 		}
 		System.out.println("LoginController:doPost Exiting...");
 	}
-		
-}
 
+}
